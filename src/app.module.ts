888fa52d8afdf4ef6f8537f758b6202e02bb4ee0
alvarado0925 <alvarado0925@gmail.com>
2025-08-26 15:09:05 -0500

    feat: upgrade NestJS and Apollo dependencies, add GraphQL support
    - Updated NestJS dependencies to version 11.x
    - Added Apollo Server and GraphQL support
    - Created HelloWorld module and resolver with a helloWorld query
    - Configured GraphQL module in app.module.ts
    - Updated main.ts to log GraphQL Playground URL
    - Generated schema.gql file automatically

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
+import { join } from 'path';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // graphiql: true, // Cambia la interfaz de usuario a GraphiQL
+      // playground: false, // Deshabilitar playground en producción
+    }),
+    HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  logger.log(`Application is running on: ${3000}`);
+  logger.log(`🚀 Application is running on: http://localhost:3000`);
+  logger.log(`🛑 GraphQL Playground: http://localhost:3000/graphql`);
 }
 bootstrap();
