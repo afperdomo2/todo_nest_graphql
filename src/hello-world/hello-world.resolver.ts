60be2786815fa948306c6659fb8e1750c6cd4937
alvarado0925 <alvarado0925@gmail.com>
2025-08-26 15:55:31 -0500

    feat: actualizar resolvers y esquema GraphQL para incluir nombres y descripciones

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,20 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: 'hello',
+    description: 'Devuelve un saludo en español',
+  })
   helloWorld(): string {
     return 'Hola mundo';
+  }
+
+  @Query(() => Float, {
+    name: 'randomNumber',
+    description: 'Devuelve un número aleatorio',
+  })
+  randomNumber(): number {
+    return Math.random() * 100;
   }
 }
