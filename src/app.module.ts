a13db8002b5b2efc127c5f0bd7bf03760c6c7512
alvarado0925 <alvarado0925@gmail.com>
2025-08-26 15:17:33 -0500

    feat: agregar soporte para la página de aterrizaje de Apollo en la configuración de GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -10,8 +11,8 @@ import { HelloWorldModule } from './hello-world/hello-world.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      // graphiql: true, // Cambia la interfaz de usuario a GraphiQL
-      // playground: false, // Deshabilitar playground en producción
+      playground: false, // Deshabilitar playground en producción
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloWorldModule,
   ],
